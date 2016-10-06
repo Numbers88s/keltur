be339ab24c2b24ae6fff25415fc4f402ac700889
Cesar Jimenez <cesar.r.jimenez@gmail.com>
2016-10-06 11:21:34 -0700

    Adding auth to server

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
-var express = require('express'),
-    port    = process.env.PORT || 3000,
-    path    = require('path'),
-    favicon = require('serve-favicon'),
-    app     = express();
+var express   = require('express'),
+    port      = process.env.PORT || 3000,
+    path      = require('path'),
+    favicon   = require('serve-favicon'),
+    basicAuth = require('basic-auth-connect'),
+    app       = express();
 
 
 app.use('/public', express.static(path.join(__dirname, '/public')));
 
 app.use(favicon(__dirname + '/public/assets/images/favicon.png'));
 
-app.get('/', function(req, res) {
+// Asynchronous Auth
+var auth = basicAuth(function(user, pass, callback) {
+  var result = (user === 'kelseybutt' && pass === 'birdsofafeather');
+  callback(null /* error */, result);
+});
+
+app.get('/', auth, function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
